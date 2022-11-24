9bc4873c69b6cdcb2c61d2209d2a6fbdc2f312f0
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2022-11-24 23:40:40 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace поликлиника
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people;
+            int timeOne = 10;
+            int chas;
+            int min;
+            Console.Write(" Введите кол-во людей ");
+            people = Convert.ToInt32(Console.ReadLine());
+            chas = people * timeOne / 60;
+            min = people * timeOne % 60;
+            Console.WriteLine(" Время ожидания " + (chas) + " часов " + (min) + " минут ");
+        }
+    }
+}
